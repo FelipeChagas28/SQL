a6bd77eed091efbbe24985a57aea3e89b4b4d778
Felipe Chagas <FelipeSChagas28@gmail.com>
2024-04-02 15:46:32 -0300

    Add files via upload

---
+++ Modelagem/Modelagem.sql
@@ -0,0 +1,19 @@
+-- Primeira forma normal
+ /*
+ 
+1 - Todo campo VETORIZADO se tornara outra tabela
+ 
+[amarelo, azul, verde]
+ 
+2 - Todo Campo multivalorado se tornara outra tabela.
+Quando o campo for divisivel 
+ 
+3 - Toda tabela necessita de pelo menos um campo que identifique todo o registro como
+sendo unico.
+isso e oq chamamos de chave primaria (PRIMARY KEY)
+ 
+ 
+ Chave natural: CPF, nome, email etc...
+ Chave artificial: IDs
+ 
+ */
